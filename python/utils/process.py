1671a5feb4da21410779b8450b7d02639af9bd01
xinroom <jiayou-xin@qq.com>
2021-03-24 14:05:33 +0800

    mod:process.py 完善

--- python/utils/process.py
+++ python/utils/process.py
@@ -1,26 +1,29 @@
 '''
 @文件    :process.py
-@说明    :进程io管理相关，主要使用环境为Windows，python38
+@说明    :进程io管理相关，主要使用环境为python38+
 @时间    :2021/02/01 11:58:55
 @作者    :XinRoom
-@版本    :1.0
+@版本    :1.1
 '''
 
 import os, sys
-from subprocess import Popen, CREATE_NEW_CONSOLE
+from subprocess import Popen
 import asyncio, threading
-import locale
 from io import BytesIO
 from time import sleep, time
-
+import platform
+THISOS = platform.system().lower()
+import locale
 DECODING = locale.getdefaultlocale()[1]
 
-# 新建一个窗体并运行
-def run_show(cmd):
-    Popen(cmd, creationflags=CREATE_NEW_CONSOLE)
+if THISOS == 'windows':
+    def run_show(cmd):
+        """新建一个窗体并运行(windwos)"""
+        from subprocess import CREATE_NEW_CONSOLE
+        Popen(cmd, creationflags=CREATE_NEW_CONSOLE)
 
-# 异步内存缓冲区
 class BytesLoop():
+    """异步内存缓冲区"""
     def __init__(self, s=b'', encoding=None):
         self._buffer = BytesIO(s)
         self._readseek = 0
@@ -47,8 +50,8 @@ class BytesLoop():
         self._lock.release()
         return writelen
 
-    # 按行读取，默认超时10s，返回可能不是完整的一行，但不影响下一行的读取
     def readline(self, timeout=10):
+        """按行读取，默认超时10s，返回可能不是完整的一行，但不影响下一行的读取"""
         _buf = b''
         _b = b''
         _st = time()
@@ -60,16 +63,16 @@ class BytesLoop():
                 _buf += _b
         return _buf
     
-    # 读取多个行，每行等待默认5s
     def readlines(self, lines: int, timeout=5):
+        """读取多个行，每行等待默认5s"""
         _buf = b''
         _lines = 0
         while (_lines:=_lines+1)<lines:
             _buf += self.readline(timeout)
         return _buf
 
-    # 读取二进制，直到匹配到untull末尾
     def readuntll(self, untll: bytes, timeout=None):
+        """读取二进制，直到匹配到untull末尾"""
         _buf = b''
         _b = b''
         _st = time()
@@ -84,10 +87,11 @@ class BytesLoop():
                 _buf += _b
         return _buf
 
-# 可以控制输入输出的异步子进程管理类
 class Cmd:
-    # cmd=执行命令，cwd=工作路径，encoding=编码格式
+    """可以控制输入输出的异步子进程管理类"""
+
     def __init__(self, cmd, cwd = None, encoding=DECODING):
+        """cmd=执行命令，cwd=工作路径，encoding=编码格式"""
         self.encoding = encoding
         self.env = os.environ
         self.env.update({"PYTHONIOENCODING": "utf-8", "LANG": "zh_CN.UTF-8"})
@@ -97,21 +101,28 @@ class Cmd:
         self.outbuf_io = BytesLoop()
         self.errbuf_io = BytesLoop()
         self.retruncode = None
+
+        interpreter = [] # 解释器
+        if THISOS == 'windows':
+            interpreter = ["cmd.exe", "/c", cmd]
+        else:
+            interpreter = ["bash", "-c", '"{}"'.format(cmd.replace('"', '\\"'))]
+             
         self._execute(
-            ["cmd.exe", "/c", cmd],
+            interpreter,
             cwd,
             lambda x: self.outbuf_io.write(x),
             lambda x: self.errbuf_io.write(x),
         )
 
-    # 输出时二进制转字符串
-    def decode(self, b):
+    def decode(self, b: bytes) -> str:
+        """输出时二进制转字符串"""
         if self.encoding == None:
             return b
         return b.decode(self.encoding, "backslashreplace")
 
-    # 开一个线程执行异步事件
-    def _execute(self, cmd, cwd, stdout_cb, stderr_cb):  
+    def _execute(self, cmd, cwd, stdout_cb, stderr_cb):
+        """开一个线程执行异步事件"""
         self.t = threading.Thread(target=asyncio.run, args=(self._stream_subprocess(
                 cmd,
                 cwd,
@@ -122,57 +133,72 @@ class Cmd:
         self.t.start()
         sleep(0.5)  # 等待子线程和子进程启动
     
-    # 杀死子进程
     def stop(self):
+        """杀死子进程"""
         if not self.isOver():
             self.subp.kill()
 
-    # 读取一行
     def readline(self):
+        """读取一行"""
         if self.isOver():
             return self.decode(self.outbuf_io.read())
         return self.decode(self.outbuf_io.readline(1))
 
-    # 实时输出执行结果
-    def print(self):
+    def print(self, timeout=None):
+        """实时输出执行结果"""
+        buf = ""
+        _st = time()
         while not self.isOver():
-            print(self.decode(self.outbuf_io.readline(1)), end='')
-        print(self.decode(self.errbuf_io.read()), end='')
-        print(self.decode(self.outbuf_io.read()), end='')
+            if timeout and time()-_st > timeout:
+                break
+            _buf = self.decode(self.outbuf_io.readline(1))
+            buf += _buf
+            print(_buf, end='')
+            sys.stdout.flush()
+        _buf = self.decode(self.errbuf_io.readline(1))
+        buf += _buf
+        print(_buf, end='')
+        _buf = self.decode(self.outbuf_io.readline(1))
+        buf += _buf
+        print(_buf, end='')
+        return buf
     
-    # 一次性读取执行结果
-    def readall(self):
+    def readall(self) -> str:
+        """一次性读取执行结果"""
         _buf = ''
         while not self.isOver():
             _buf += self.decode(self.outbuf_io.readline(1))
-        _buf += self.decode(self.errbuf_io.read())
+        _buf +=  self.decode(self.errbuf_io.read())
         _buf += '\n'
-        _buf += self.decode(self.outbuf_io.read())
+        _buf +=  self.decode(self.outbuf_io.read())
         return _buf
     
-    # 一次性读取执行结果，二进制形式
-    def readallbyte(self):
+    def readallbyte(self) -> bytes:
+        """一次性读取执行结果，返回二进制形式"""
         _buf = b''
         while not self.isOver():
             _buf += self.outbuf_io.readline(1)
         _buf += self.outbuf_io.read()
         return _buf
     
-    # 向子进程输入内容
-    def write(self, data):
+    def send(self, data):
+        """向子进程输入内容"""
         self.subp.stdin.write(data.encode(self.encoding))
     
-    # 向子进程输入内容并回车
-    def writeline(self, data):
-        self.write(data + "\r\n")
+    def sendline(self, data):
+        """向子进程输入内容并回车"""
+        if THISOS == 'windows':
+            self.send(data + "\r\n")
+        else:
+            self.send(data + "\n")
 
-    # 异步流传递
     async def _read_stream(self, stream, cb):
+        """异步流传递"""
         while (_b := await stream.read(1)):
             cb(_b)
 
-    # 异步子进程
     async def _stream_subprocess(self, cmd, cwd, stdout_cb, stderr_cb):
+        """异步子进程"""
         self.subp = await asyncio.create_subprocess_exec(*cmd,
                 env=self.env, cwd=cwd,
                 stdin=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
@@ -183,15 +209,18 @@ class Cmd:
         ])
         return await self.subp.wait()
 
-    # 判断子进程是否执行完毕
     def isOver(self):
+        """判断子进程是否执行完毕"""
         self.retruncode = self.subp.returncode
         return self.retruncode != None
 
 
 if __name__ == '__main__':
     # 测试使用
-    _p = Cmd('cmd.exe')
-    _p.writeline('whoami')
-    _p.writeline('exit')
+    if THISOS == 'windows':
+        _p = Cmd('cmd.exe')
+    else:
+        _p = Cmd('bash')
+    _p.sendline('whoami')
+    _p.sendline('exit')
     _p.print()
